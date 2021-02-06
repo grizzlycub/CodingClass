c82c7d9afc1abadf116831d907a6603d4ed672ee
Austin Williamson <austin@keepintouchwith.us>
2021-02-06 14:59:43 -0800

    Add 2 initial projects for madlibs

---
+++ project 1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sillyName = PromptUserForValue("What is your name?");
+            string game = PromptUserForValue("What is your favorite game to play outside?");
+            string tool = PromptUserForValue("Name a tool in your shop?");
+            string adjective1 = PromptUserForValue("Type an adjective:");
+            string playmate = PromptUserForValue("What did you eat for dinner last night?");
+            string petName = PromptUserForValue("How do you prefer eggs cooked?");
+            string pastTenseVerb = PromptUserForValue("Type a past tense verb that ends in -ed:");
+            string place = PromptUserForValue("Name a place.");
+            string verb1 = PromptUserForValue("Type an action verb.");
+
+            Console.WriteLine("Press any key to see the story you made.");
+            Console.ReadKey();
+
+            Console.Clear();
+            Console.WriteLine("And now for our story...");
+            Console.WriteLine($"One day, a young boy named {sillyName} was playing {game} using a {tool} with his {adjective1} friend {playmate}!");
+            Console.WriteLine($"His pet {petName} came over and {pastTenseVerb} {playmate}.");
+            Console.WriteLine($"Then {playmate} had to go to {place} to {verb1}!");
+
+            Console.WriteLine("The END!");
+        }
+
+        private static string PromptUserForValue(string question)
+        {
+            ConsoleColor originalForegroundColor = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(question);
+            Console.ForegroundColor = ConsoleColor.White;
+            string result = Console.ReadLine();
+
+            Console.ForegroundColor = originalForegroundColor;
+
+            return result;
+        }
+    }
+}
---
+++ project 2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string verbIng1 = PromptUserForValue("Type an action verb ending in -ing.");
+            string adjective1 = PromptUserForValue("Type an adjective.");
+            string verbIng2 = PromptUserForValue("Type an action verb ending in -ing.");
+            string partOfBody1 = PromptUserForValue("Name a part of your body.");
+            string adverb = PromptUserForValue("Type an adverb (e.g. faintly).");
+            string partOfBody2 = PromptUserForValue("Name a part of your body.");
+            string noun = PromptUserForValue("Type a noun");
+            string verb = PromptUserForValue("Type a verb");
+            string animals = PromptUserForValue("Type an animal but it has to be plural");
+            string color = PrtypomptUserForValue("Type a color");
+            string adjective = PromptUserForValue("Type  an ajective");
+            Console.ReadKey();
+
+            Console.Clear();
+            Console.WriteLine($"BIKE RIDING!!!");
+            Console.WriteLine($"Most doctors agree that bicycle {verbIng1} is a/an {adjective1} form of exercise.");
+            Console.WriteLine($"{verbIng2} a bicycle enables you to develop your {partOfBody1} muscles as well as {adverb} increase the rate of your {partOfBody2} beat.");
+            Console.WriteLine($"More {noun} around the world {verb} bicycles than drive {animals}  ");
+            Console.WriteLine($"No mater what kind of {noun} you {verb} allways be shure to ware a {adjective} helmet.");
+            Console.WriteLine($"Make sure to have {color} reflectors too !");
+            Console.WriteLine($"The end");
+        
+            
+        }
+
+        private static string PromptUserForValue(string question)
+        {
+            ConsoleColor originalForegroundColor = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(question);
+            Console.ForegroundColor = ConsoleColor.White;
+            string result = Console.ReadLine();
+
+            Console.ForegroundColor = originalForegroundColor;
+
+            return result;
+        }
+    }
+}
